2cce1dcdb537d33ac8134089b47eee8a2f547291
karthik-A-22 <karthik.a2219905@gmail.com>
2024-08-01 19:31:12 +0530

    "Removed 'libraries' from route paths in libraryRoutes.js"

--- routes/libraryRoutes.js
+++ routes/libraryRoutes.js
@@ -15,15 +15,15 @@ const { validateRole } = require('../middleware/role');
 const router = express.Router();
 
 // Library routes
-router.get('/libraries', authenticate, getLibraries);
-router.get('/libraries/:id', authenticate, getLibraryById);
-router.post('/libraries', authenticate, validateRole('Admin'), createLibrary);
-router.put('/libraries/:id', authenticate, validateRole('Admin'), updateLibrary);
-router.delete('/libraries/:id', authenticate, validateRole('Admin'), deleteLibrary);
+router.get('/', authenticate, getLibraries);
+router.get('/:id', authenticate, getLibraryById);
+router.post('/', authenticate, validateRole('Admin'), createLibrary);
+router.put('/:id', authenticate, validateRole('Admin'), updateLibrary);
+router.delete('/:id', authenticate, validateRole('Admin'), deleteLibrary);
 
 // Library inventory routes
-router.get('/libraries/:id/inventory', authenticate, getLibraryInventory);
-router.post('/libraries/:id/inventory', authenticate, validateRole('Admin'), addBookToInventory);
-router.delete('/libraries/:id/inventory/:bookId', authenticate, validateRole('Admin'), removeBookFromInventory);
+router.get('/:id/inventory', authenticate, getLibraryInventory);
+router.post('/:id/inventory', authenticate, validateRole('Admin'), addBookToInventory);
+router.delete('/:id/inventory/:bookId', authenticate, validateRole('Admin'), removeBookFromInventory);
 
 module.exports = router;
