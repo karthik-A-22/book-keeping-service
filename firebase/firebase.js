a0138bc659a341e5744bc35e1f883fe03f92f0f0
karthik-A-22 <karthik.a2219905@gmail.com>
2025-01-10 15:23:26 +0530

    commit

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,6 +1,6 @@
 // firebase/firebase.js
 const admin = require('firebase-admin');
-const serviceAccount = require('../firebase.config');
+const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT_KEY;
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
